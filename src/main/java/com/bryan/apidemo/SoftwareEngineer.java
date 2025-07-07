3733645a21cbfc1676c91175100dcd540bf95d46
Bryan Sandoval <cebrerosbryan@gmail.com>
2025-07-06 22:27:50 -0700

    auto increment

--- src/main/java/com/bryan/apidemo/SoftwareEngineer.java
+++ src/main/java/com/bryan/apidemo/SoftwareEngineer.java
@@ -1,6 +1,8 @@
 package com.bryan.apidemo;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 import java.util.List;
@@ -10,6 +12,7 @@ import java.util.Objects;
 public class SoftwareEngineer {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer Id;
     private String name;
     private String techStack;
